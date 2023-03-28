86ea5b9a0cd2c6f8efeb1836ad8c9a7c0fcb9222
Onalenna Junior Makhura <ojmakhura@roguesystems.co.bw>
2023-03-28 09:55:12 +0200

    add admin client

---
+++ admin/src/test/java/bw/co/roguesystems/admin/IthutengAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bw.co.roguesystems.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IthutengAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
